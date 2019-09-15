3c7bb1cea4dd9504f0e22664486794364bbed7df
steven czarnecki <rdgNHL@gmail.com>
2019-09-14 22:41:47 -0500

    code for ola4

--- cs3180/current/Czarnecki4.cpp
+++ cs3180/current/Czarnecki4.cpp
@@ -3,6 +3,7 @@
 
   Lab number: Closed Lab 4 
   Purpose: Remove loss of significance from function
+  		   With single and double precision
   Due date: 09/12/2019
   */
 
@@ -13,35 +14,64 @@
 using namespace std;
 
 //function prototypes
-void function1(double x);
-void function2(double x);
+void single_f1(float x);
+void single_f2(float x);
+void double_f1(double x);
+void double_f2(double x);
 
 //main
 int main()
 {
-	double x, n;
+	float x, n;
+	double y;
 	cout << "Enter the number for x: ";
 	cin >> x;
 	cout << "Enter the number of iterations: ";
 	cin >> n;
+	//set for 2nd loop
+	y = x;
 
-	cout << "x\t\t f(x)\t\t f2(x)\n";
+	//print header
+	cout << "single precision\nx\t\t f(x)\t\t f2(x)\n";
 	for(int i = 0; i < n; i++)
 	{
-		function1(x, n, i);
-		function2(x, n, i);
+		//prints subtraction and then addition version of formula
+		//single precision
+		single_f1(x);
+		single_f2(x);
+		//increments by power of 10 per run
 		x = x*10;
+	}
+
+	//2nd print header
+	cout << "\ndouble precision\nx\t\t f(x)\t\t f2(x)\n";
+	for(int i = 0; i < n; i++)
+	{
+		//print same as above but with double precission
+		double_f1(y);
+		double_f2(y);
+		y = y*10;
 	}
 	return 0;
 }
 
 //functions
-void function1(double x)
+//all single and double versions
+void single_f1(float x)
 {
 	cout << x << "\t\t" << (x * (sqrt(x+1) - sqrt(x)));
 }
 
-void function2(double x)
+void single_f2(float x)
 {
-	cout << "\t\t" << (x/(sqrt(x+1) + x));
+	cout << "\t\t" << (x/(sqrt(x+1) + sqrt(x)));
+}
+void double_f1(double x)
+{
+	cout << x << "\t\t" << (x * (sqrt(x+1) - sqrt(x)));
+}
+
+void double_f2(double x)
+{
+	cout << "\t\t" << (x/(sqrt(x+1) + sqrt(x)));
 }
