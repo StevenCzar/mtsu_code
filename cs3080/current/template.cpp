80d712148e281918a786a917cb7e4dfe63421b30
steven czarnecki <rdgNHL@gmail.com>
2019-11-04 18:38:27 -0600

    stuff

--- cs3080/current/template.cpp
+++ cs3080/current/template.cpp
@@ -1,9 +1,9 @@
 /*Author: Steven Czarnecki
   Course: CSCI 3080
 
-  Lab number: Lab 2
-  Purpose: Matrix Math
-  Due date: 10/04/2019
+  Lab number: Lab 3
+  Purpose: Gaussian Elimination
+  Due date: 11/04/2019
   */
 
 //dependencies
@@ -12,206 +12,67 @@
 using namespace std;
 
 //main
-int main()
+main()
 {
-	//initialize variables
-	int rowA, rowB, colA, colB;
-	int matA[10][10], matB[10][10];
-	char choice;
-	//use for check on validation for operations
-	bool valid = true;
+	//initialize things
+	float mat[10][11];
+	int size;
+	float temp, multi;
+	int i, j, k;
 
-	//initial input
-	cout << "Input the row size of A: ";
-	cin >> rowA;
-	cout << "Input the column size of A: ";
-	cin >> colA;
-	for(int i = 0; i < rowA; i++)
+	//get size of square
+	cout << "Input the number of rows: ";
+	cin >> size;
+
+	//matrix goes here
+	cout << "Input the matrix: \n";
+	for(i = 0; i < size; i++)
 	{
-		for(int j = 0; j < colA; j++)
+		for(j = 0; j < size; j++)
 		{
-			//prompt for each individual element of the matrix
-			cout << "Input matrix A (element [" << i+1 << "][" << j+1 << "]): ";
-			cin >> matA[i][j];
+			cin >> mat[i][j];
 		}
 	}
 
-	//still inputing
-	cout << "Input the row size of B: ";
-	cin >> rowB;
-	cout << "Input the column size of B: ";
-	cin >> colB;
-	for(int i = 0; i < rowB; i++)
+	//answers go in last column
+	cout << "Input the answer column: \n";
+	for(i = 0; i < size; i++)
 	{
-		for(int j = 0; j < colB; j++)
-		{
-			cout << "Input matrix rowB(element [" << i+1 << "][" << j+1 << "]): ";
-			cin >> matB[i][j];
-		}
-	}	
+		cin >> mat[i][size];
+	}
 
-	//input char for choice
-	cout << "Choose your operation: A for add, S for subtract, ";
-	cout << "M for multiply, N for AND, O for OR\n";
-	cin >> choice;
+	//do gaussian
+	for(i =0; i < size; i++)
+	{
+		temp = mat[i][i];
+		//gotta make it size+1 to include answer column
+		for(j = 0; j < size+1; j++)
+		{
+			//divide each element in row by element in diagonal
+			mat[i][j] = (mat[i][j]/temp);
+		}
+		//reset j
+		j=0;
+		//for each row j not equal to i
+		for(j = 0; j < size; j++)
+		{
+			if(j!=i)
+			{
+				multi = -mat[j][i];
+				//gotta be size+1 for columns
+				for(k = 0; k < size+1; k++)
+				{
+					mat[j][k] = mat[j][k] + (mat[i][k]*multi);
+				}
+			}
+		}
 
-	//if-else segment for char check
-	//if adding....
-	if(choice == 'A' || choice == 'a')
-	{
-		//validation for operation
-		if((rowA != rowB)||(colA != colB))
-		{
-			cout << "Invalid dimensions for operation" << endl;
-			valid = false;
-		}
-		//if you can do it, then do math
-		if(valid)
-		{
-			cout << "The answer is: \n";
-			//add element by element
-			for(int i = 0; i < rowA; i++)
-			{
-				for(int j = 0; j < colA; j++)
-				{
-					cout <<  matA[i][j] + matB[i][j];
-					cout << " ";
-				}
-				cout << "\n";
-			}
-		}
 	}
-	//subtraction time
-	else if(choice == 'S' || choice == 's')
+
+	//prlast column which should have answers maybe
+	cout << "The final answers are: \n";
+	for(i = 0; i < size; i++)
 	{
-		//validation time
-		if((rowA != rowB)||(colA != colB))
-		{
-			cout << "Invalid dimensions for operation" << endl;
-			valid = false;
-		}
-		//if valid, then do thing
-		if(valid)
-		{
-			cout << "The answer is: \n";
-			//subtract each element by each element
-			for(int i = 0; i < rowA; i++)
-			{
-				for(int j = 0; j < colA; j++)
-				{
-					cout <<  matA[i][j] - matB[i][j];
-					cout << " ";
-				}
-				cout << "\n";
-			}
-		}
+		cout >> mat[i][size] >> endl;
 	}
-	//multiply time
-	else if(choice == 'M' || choice == 'm')
-	{
-		//declare temp array for storage of values
-		int temp[10][10];
-		//validation time
-		if(rowA!=colB)
-		{
-			cout << "Invalid dimensions for operation" << endl;
-			valid = false;
-		}
-		//if it works, then do thing
-		if(valid)
-		{
-			cout << "The answer is: \n";
-			//multiply time for real
-			for(int i = 0; i < rowA; i++)
-			{
-				for(int j = 0; j < colA; j++)
-				{
-					temp[i][j] = 0;
-					for(int k = 0; k < colA; k++)
-					{
-						//keeps track of sum of each multiplication operation
-						temp[i][j] += (matA[i][k] * matB[k][j]);
-					}
-					//print final element
-					cout << temp[i][j];
-					cout << " ";
-				}
-				cout << "\n";
-			}
-		}
-	}
-	//AND time
-	else if(choice == 'N' || choice == 'n')
-	{
-		//temp array since it's the same as multiplying
-		int temp[10][10];
-		if(rowA!=colB)
-		{
-			cout << "Invalid dimensions for operation" << endl;
-			valid = false;
-		}
-		//same as multiply
-		if(valid)
-		{
-			cout << "The answer is: \n";
-			for(int i = 0; i < rowA; i++)
-			{
-				for(int j = 0; j < colA; j++)
-				{
-					temp[i][j] = 0;
-					for(int k = 0; k < colA; k++)
-					{
-						temp[i][j] += (matA[i][k] * matB[k][j]);
-					}
-					//literally the same thing as multiplying, but if the value is 1 or more, make it a 1
-					if(temp[i][j] >= 1)
-					{
-						temp[i][j] = 1;
-					}
-					cout << temp[i][j];
-					cout << " ";
-				}
-				cout << "\n";
-			}
-		}
-	}
-	//OR time
-	else if(choice == 'O' || choice == 'o')
-	{
-		//validation time
-		if((rowA != rowB)||(colA != colB))
-		{
-			cout << "Invalid dimensions for operation" << endl;
-			valid = false;
-		}
-		//last time it does things
-		if(valid)
-		{
-			cout << "The answer is: \n";
-			for(int i = 0; i < rowA; i++)
-			{
-				for(int j = 0; j < colA; j++)
-				{
-					//doesn't even put into an output array.  if value after adding is 1 or more, makes it 1
-					if((matA[i][j] + matB[i][j]) >= 1)
-					{
-						cout << "1";
-					}
-					else
-					{
-						cout << "0";
-					}
-					cout << " ";
-				}
-				cout << "\n";
-			}
-		}
-	}
-	//if not a valid char selection... tell them they did it wrong
-	else
-	{
-		cout << "Invalid choice\n";
-	}
-	//the end
-	return 0;
 }
--- cs3180/current/template.cpp
+++ cs3180/current/template.cpp
@@ -1,21 +1,156 @@
 /*Author: Steven Czarnecki
   Course: CSCI 3180
 
-  Lab number: Lab 
-  Purpose: 
+  Lab number: Lab 2
+  Purpose: Eulers/
   Due date: /2019
   */
 
 //dependencies
-#include
+#include <iostream>
+#include <cmath>
 using namespace std;
 
 //function prototypes
+/*menu - basically does everything
+  from print statements to calling the right ODE's*/
+bool menu();
+{
+	double h, t, x, temp, interval, y, soln;
+	int n, lower, upper;
+	char choice;
+	cout << "Which ODE would you like to use?\n";
+	cout << "1. x' = x / (1+t)\n";
+	cout << "2. x' = t + x\n";
+	cout << "3. x' = t - x\n";
+	cout << "4. x' = (t - x) / (t + x)\n";
+	cout << "Enter any other value to end\n";
+	cout << "Choice: ";
+	cin >> choice;
+
+	if!(choice = "1"||choice == "2"||choice=="3"||choice =="4")
+		return false;
+
+	cout << "Enter the lower bound: ";
+	cin >> lower;
+	cout << "Enter the upper bound: ";
+	cin >> upper;
+	cout << "Enter the starting value: ";
+	cin >> x;
+	cout << "Enter the number of sub intervals: ";
+	cin >> n;
+
+
+	h = (upper-lower)/(n);
+	t = lower;
+	if(choice == "1")
+	{
+		cout << "Analytical Solution: " << soln << endl;
+		for(int i = 1; i <= n; i++)
+		{
+			temp = t + (h*first(x,t));
+			if((i <= 9)||(i == n))
+			{
+				cout << t << "\t\t" << temp << endl;
+			}
+			t = temp;
+			x+=h;
+		}
+	}
+	else if(choice == "2")
+	{
+		cout << "Analytical Solution: " << soln << endl;
+		for(int i = 1; i <= n; i++)
+		{
+			temp = t + (h*second(x,t));
+			if((i <= 9)||(i == n))
+			{
+				cout << t << "\t\t" << temp << endl;
+			}
+			t = temp;
+			x+=h;
+		}
+	}
+	else if(choice == "3")
+	{
+		cout << "Analytical Solution: " << soln << endl;
+		for(int i = 1; i <= n; i++)
+		{
+			temp = t + (h*third(x,t));
+			if((i <= 9)||(i == n))
+			{
+				cout << t << "\t\t" << temp << endl;
+			}
+			t = temp;
+			x+=h;
+		}
+	}
+	else(choice == "4")
+	{
+		cout << "Analytical Solution: " << soln << endl;
+		for(int i = 1; i <= n; i++)
+		{
+			eulers = t + (h*fourth(x,t));
+			if((i <= 9)||(i == n))
+			{
+				cout << t << "\t\t" << eulers << endl;
+			}
+			t = eulers;
+			x+=h;
+		}
+	}
+	return true;
+}
+
+double eulers(Function
+{
+
+}
+
+double heuns()
+{
+
+}
+
+double runge_kutta()
+{
+
+}
+
+double first(double x, double t);
+{
+	double est;
+	est = (x/(1+t));
+	return est;
+}
+double second(double x, double t);
+{
+	double est;
+	est = t+x;
+	return est;
+}
+double third(double x, double t);
+{
+	double est;
+	est = t-x;
+	return est;
+}
+double fourth(double x, double t);
+{
+	double est;
+	est = (t-x)/(t+x);
+	return est;
+}
 
 //main
 int main()
 {
-
+	bool validation;
+	validation = true;
+	do
+	{
+		validation = menu();
+	}while(validation);	
 	return 0;
 }
 
