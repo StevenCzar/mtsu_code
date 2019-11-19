7d19fb612396a8f1d67562949265b8756dc1b026
steven czarnecki <rdgNHL@gmail.com>
2019-11-18 18:47:30 -0600

    something new

--- cs3080/current/ola4.cpp
+++ cs3080/current/ola4.cpp
@@ -1,84 +1,97 @@
 /*Author: Steven Czarnecki
   Course: CSCI 3080
 
-  Lab number: Lab 4
-  Purpose: Array-Pointer Representation to other things
-  Due date: 11/11/2019
+  Lab number: Lab 5
+  Purpose: Tree array representation traversials
+  Due date: 11/18/2019
   */
 
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
+void preorder(int tree[], int root)
+void inorder(int tree[], int root)
+void postorder(int tree[], int root)
+
 int main()
 {
-	//initialize things
-	int adj[10][10];
-	int apr[20][2];
-	int final[10];
-	int i, j, numNodes, numRows, temp, degree;
-	//make matrix 0's
-	for(i = 0; i < 10; i++)
-	{
-		for(j = 0; j < 10; j++)
-		{
-			adj[i][j] = 0;
-		}
-	}
-	
-	//basic i/o stuff
+	int tree[10][3];
+	int nodes, i, j;
+
 	cout << "Please input the number of nodes: ";
-	cin >> numNodes;
-	cout << "Please input the number of rows in the matrix: ";
-	cin >> numRows;
-
-	cout << "Please input the array-pointer representation of the graph: \n";
-	for(i=0; i<numRows; i++)
+	cin >> nodes;
+	cout << "Please input the tree array representation of the graph: ";
+	for(i=0; i<nodes; i++)
 	{
-		for(j=0; j<2; j++)
+		for(j=0; j<3; j++)
 		{
-			cin >> apr[i][j];
+			cin >> tree[i][j];
 		}
 	}
 
-	//create adjacency matrix from pseudocode
-	for(i=0; i<numRows;i++)
-	{
-		temp = apr[i][1];
-		while(temp!=0)
-		{
-			adj[i][apr[temp][0]] = 1;
-			temp = apr[temp][1];
-		}
-	}
+	cout << "\nThe preorder traversal is: \n";
+	preorder(tree, 0);
+	cout << "\n The inorder traversial is: \n";
+	inorder(tree, 0);
+	cout << "\n the postorder traversial is: \n";
+	postorder(tree, 0);
 
-	//from adjacency matrix, get degree and each adjacent node
-	for(i=0; i<numNodes; i++)
-	{
-		cout << "\n***Node " << i+1 << " information***\n";
-		degree = 0;
-		for(j=0; j<numNodes; j++)
-		{
-			if(adj[i][j] == 1)
-			{	
-				//gets adjacent node and places in array
-				final[degree] = j+1;
-				degree++;
-			}
-		}
-		//if the degree of the node is 0, print that it is isolated
-		if(degree == 0)
-		{
-			cout << i+1 << " is an isolated node \n";
-		}
-		//print out the degree of the node
-		cout << "Degree of node " << i+1 << " is: " << degree << endl;
-		//print the adjacent nodes
-		cout << "The nodes adjacent to node " << i+1 << " are: ";
-		for(j=0;j<=degree;j++)
-		{
-			cout << final[j] << " ";
-		}
-	}
 	return 0;
+}
+
+//root, left, middle, right
+void preorder(int tree[], int root)
+{
+	cout << root+1 << " ";
+	if(tree[root][0]!=0)
+	{
+		preorder(tree, tree[root][0]);
+	}
+	if(tree[root][1]!=0)
+	{
+		preorder(tree, tree[root][1]);
+	}
+	if(tree[root][2]!=0)
+	{
+		preorder(tree, tree[root][2]);
+	}
+	return;
+}
+
+//left, root, middle, right
+void inorder(int tree[], int root)
+{
+	if(tree[root][0]!=0)
+	{
+		inorder(tree, tree[root][0]);
+	}
+	cout << root+1 << " ";
+	if(tree[root][1]!=0)
+	{
+		inorder(tree, tree[root][1]);
+	}
+	if(tree[root][2]!=0)
+	{
+		inorder(tree, tree[root][2]);
+	}
+	return;
+}
+
+//left, middle, right, root
+void postorder(int tree[], int root)
+{
+	if(tree[root][0]!=0)
+	{
+		postorder(tree,tree[root][0]);
+	}
+	if(tree[root][1]!=0)
+	{
+		postorder(tree,tree[root][1]);
+	}
+	if(tree[root][2]!=0)
+	{
+		postorder(tree,tree[root][2]);
+	}
+	cout << root+1 << " ";
 }
